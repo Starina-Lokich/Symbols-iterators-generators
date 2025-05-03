f4300589e89499f51a4332f38153364935ed1576
Starina-Lokich <starinalokich@gmail.com>
2025-05-03 15:20:46 +0300

    feat: initialize project with package.json and webpack configuration - Added package.json with essential scripts, dependencies, and metadata. - Configured webpack with Babel for JavaScript transpilation and HtmlWebpackPlugin for HTML file generation.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            sourceType: 'unambiguous'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  mode: 'production',
+  resolve: {
+    fullySpecified: false
+  }
+};
